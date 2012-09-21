886acae4efbdcd72da45bf7d9f51768f1ac4101c
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-09-21 22:08:12 +0200

    add header and fix getValue bug

--- src/jquery.editablearea.js
+++ src/jquery.editablearea.js
@@ -1,3 +1,26 @@
+/**
+ * jQuery editable area plugin
+ * Copyright (C) 2012  Bertrand CHEVRIER, KrampStudio
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/> 
+ * 
+ * @author <a href="mailto:chevrier.bertrand@gmail.com">Bertrand Chevrier</a>
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @version $version$
+ * @requires jquery >= 1.7
+ */
+
 (function( $ ){
     "use strict";
     
@@ -138,7 +161,7 @@
             return this.each(function() {
                 var $elt = $(this);
                 if(EditableArea._isEditing($elt.attr('id'))){
-                    value = value || $elt.editableArea('getValue');
+                    value = value || $elt.editableArea('getValues');
                     var data = $elt.data('editableArea');
                     $elt.empty();
                     switch(data.type){
